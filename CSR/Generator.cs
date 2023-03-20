1aa5d665f81ff090316310d65167e47eef034378
Andrey Didenko <a.rudskoj@icloud.com>
2023-03-20 12:23:24 +0300

    Generatot added

---
+++ CSR/Generator.cs
@@ -0,0 +1,10 @@
+﻿using FiniteFields;
+
+namespace CSR
+{
+    public class Generator
+    {
+        private FiniteField field = new FiniteField(2,8,new int[] { 1, 1, 0, 1, 1, 0, 0, 0, 1 });
+        
+    }
+}
---
+++ CSR/Program.cs
@@ -0,0 +1,7 @@
+﻿namespace CSR
+{
+    class Program
+    {
+        public void Main(string[] args) { }
+    }
+}
