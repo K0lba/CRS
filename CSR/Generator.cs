6ab478f5cd6c7c00db8092a24c2c22f26fbae078
Andrey Didenko <a.rudskoj@icloud.com>
2023-03-21 23:09:04 +0300

    Added Generator and method Generate

--- CSR/Generator.cs
+++ CSR/Generator.cs
@@ -5,6 +5,34 @@ namespace CSR
     public class Generator
     {
         private FiniteField field = new FiniteField(2,8,new int[] { 1, 1, 0, 1, 1, 0, 0, 0, 1 });
-        
+        private int k { get; set; }
+        private FiniteFieldElements[] Coef { get; set; }
+        private FiniteFieldElements C { get; set; }
+        private FiniteFieldElements[] InitialVar { get; set; }
+        public Generator(byte[] c, List<byte[]> coef, List<byte[]> initVal)
+        {
+            C = field.CreateElementFromBinary(c);
+            k = coef.Count;
+            Coef = new FiniteFieldElements[k];
+            InitialVar = new FiniteFieldElements[k];
+            for (int i = 0; i < k; i++)
+            {
+                Coef[i] = field.CreateElementFromBinary(coef[i]);
+                InitialVar[i] = field.CreateElementFromBinary(initVal[i]);
+            }
+        }
+        public byte[] Generate()
+        {
+            FiniteFieldElements x_n = new FiniteFieldElements(new int[] { 0 }, field);
+            for (int i = 0; i <= k - 1; i++)
+            {
+                x_n += Coef[i] * InitialVar[i];
+            }
+            x_n += C;
+            for (int i = 0; i < InitialVar.Length - 1; i++)
+                InitialVar[i] = InitialVar[i + 1];
+            InitialVar[InitialVar.Length] = x_n;
+            return x_n.GetToBinary();
+        }
     }
 }
